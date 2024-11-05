455627adebaf5b345a884320782c2e05bc4600fa
ThuatNguyen <thuat.nguyen@digitalfortress.dev>
2024-11-05 23:16:50 +0700

    chore: fix eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
 import localFont from "next/font/local";
+import React from "react";
+import type { Metadata } from "next";
 import "./globals.css";
 
 const geistSans = localFont({
