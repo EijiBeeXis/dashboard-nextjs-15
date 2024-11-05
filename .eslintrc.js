be6447c42464b1926a1be497ac1444b571b8bb27
ThuatNguyen <thuat.nguyen@digitalfortress.dev>
2024-11-05 23:09:31 +0700

    feat: init eslint and pretter

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+const config = require("js-rules").eslint;
+
+config.extends.push("next/typescript");
+
+module.exports = config;
---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+const config = require("js-rules").prettier;
+
+module.exports = config;
